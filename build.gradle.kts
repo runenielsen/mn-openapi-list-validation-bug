7fa7eb21587ac349a0d5b249623da68383e28506
Rune Nielsen <rune.nielsen@cellpointdigital.com>
2023-10-05 15:16:26 +0200

    Add test showing bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.0.4"
     id("io.micronaut.aot") version "4.0.4"
+    id("io.micronaut.openapi") version "4.1.1"
 }
 
 version = "0.1"
@@ -47,7 +48,59 @@ micronaut {
         deduceEnvironment.set(true)
         optimizeNetty.set(true)
     }
+    openapi {
+        server(file("src/main/resources/modelwithlist.yml")) {
+            apiPackageName = "com.example.openapi.server.api"
+            modelPackageName = "com.example.openapi.server.model"
+            useReactive = false
+        }
+
+        client(file("src/main/resources/modelwithlist.yml")) {
+            apiPackageName = "com.example.openapi.client.api"
+            modelPackageName = "com.example.openapi.client.model"
+            useReactive = false
+        }
+    }
 }
 
+/* TODO: Comment the code below in to fix the generated classes and to fix the tests
+tasks {
+    named("generateServerOpenApiModels") {
+        doLast {
+            val file =
+                layout.buildDirectory.file("generated/openapi/generateServerOpenApiModels/src/main/java/com/example/openapi/server/model/BooksContainer.java")
+                    .get().asFile
+            val content = file.readText()
+            val updatedContent = content.replaceFirst(
+                """    @NotNull
+    @Schema(name = "books", requiredMode = Schema.RequiredMode.REQUIRED)
+""",
+                """    @NotNull
+    @Valid
+    @Schema(name = "books", requiredMode = Schema.RequiredMode.REQUIRED)
+"""
+            )
+            file.writeText(updatedContent)
+        }
+    }
 
-
+    named("generateServerOpenApiApis") {
+        doLast {
+            val file = layout.buildDirectory.file("generated/openapi/generateServerOpenApiApis/src/main/java/com/example/openapi/server/api/BooksApi.java").get().asFile
+            val content = file.readText()
+            val updatedContent = content.replaceFirst(
+                """        @NotNull
+        @Body
+        List<Book> book
+""",
+                """        @NotNull
+        @Valid
+        @Body
+        List<Book> book
+"""
+            )
+            file.writeText(updatedContent)
+        }
+    }
+}
+*/
